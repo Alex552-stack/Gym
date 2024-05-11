b3a51cd1af44a15ce0cd47c604f111e2bab0e1ba
Iustinia <154070315+Iustinia@users.noreply.github.com>
2024-05-11 20:41:40 +0300

    QuickFix MaxWith App.tsx

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -38,7 +38,7 @@ export default function App() {
       <ResponsiveAppBar/>
       {loading ? <LoadinComponent message="Loading..." />
           : locaiton.pathname === '/' ? <HomePage />
-          : <Container sx={{mx:0, my: 0, px: 0 , py: 0, pl: 0, pr: 0}}>
+          : <Container maxWidth={false} sx={{mx:0, my: 0, px: 0 , py: 0, pl: 0, pr: 0}}>
               <Outlet />
            </Container>
       }
