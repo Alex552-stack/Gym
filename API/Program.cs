6d849dd98c8945ace3f01e5e67a9282d350fe8d9
Alex <avramalexandru2030@gmail.com>
2024-06-18 16:38:21 +0300

    .

--- API/Program.cs
+++ API/Program.cs
@@ -19,26 +19,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string connString;
-if (builder.Environment.IsDevelopment())
+// z
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else
 {
-    // Use connection string provided at runtime by FlyIO.
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+//     // Use connection string provided at runtime by FlyIO.
+//     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-    var updatedHost = pgHost.Replace("flycast", "internal");
+//     // Parse connection URL to connection string for Npgsql
+//     connUrl = connUrl.Replace("postgres://", string.Empty);
+//     var pgUserPass = connUrl.Split("@")[0];
+//     var pgHostPortDb = connUrl.Split("@")[1];
+//     var pgHostPort = pgHostPortDb.Split("/")[0];
+//     var pgDb = pgHostPortDb.Split("/")[1];
+//     var pgUser = pgUserPass.Split(":")[0];
+//     var pgPass = pgUserPass.Split(":")[1];
+//     var pgHost = pgHostPort.Split(":")[0];
+//     var pgPort = pgHostPort.Split(":")[1];
+//     var updatedHost = pgHost.Replace("flycast", "internal");
 
-connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+// connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 
 builder.Services.AddDbContext<GymDbContext>(
